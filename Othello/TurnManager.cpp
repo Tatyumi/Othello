8cc64b8ffcf139e55ec116808cf7d201593ef956
Tatyumi <s150204@nishitech.ac.jp>
2019-10-23 22:19:06 +0900

    新規追加

---
+++ Othello/TurnManager.cpp
@@ -0,0 +1,8 @@
+#include "TurnManager.h"
+
+
+CTurnManager::CTurnManager()
+{
+	SetTurnStrs(BLACK, "Black");
+	SetTurnStrs(WHITE, "White");
+}
---
+++ Othello/TurnManager.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+// ターンによって指定される色
+enum ETurnColors
+{
+	BLACK,
+	WHITE,
+};
+
+// 色の名前文字数
+const int colorNameCount = 5;
+
+// nullポインタ領域
+const int nullPtr = 1;
+
+// ターンで指定される色の種類
+const int turnColors = WHITE + 1;
+
+class CTurnManager
+{
+public:
+	// コンストラクタ
+	CTurnManager();
+
+
+	// turnStrsのアクセサ
+	//void SetTurnStrs(int turnColor, const char* colorName) { turnStrs[turnColor][colorNameCount + nullPtr] = colorName; };
+	void SetTurnStrs(int turnColor, const char* colorName) { turnStrs[turnColor] = colorName; };
+
+private:
+	// ターン表示
+	//const char* turnStrs[turnColors][colorNameCount + nullPtr];
+	const char* turnStrs[turnColors];
+
+	int turn;
+};
+
