7e8581f512c5b8b98c6aff131eddb8cefddb145f
Tatyumi <s150204@nishitech.ac.jp>
2019-10-24 22:37:47 +0900

    石が置けるか判別する処理を追加
    石が置く際に、延長線上に同じ色の石があるか判別を行い、石の配置、またひっくり返す処理を実装した。

--- Othello/StoneManager.cpp
+++ Othello/StoneManager.cpp
@@ -1,5 +1,19 @@
 ﻿#include<conio.h>
 #include "StoneManager.h"
+#include "BoardManager.h"
+#include "TurnManager.h"
+
+// 方向の種類
+int directions[][2] = {
+		{ 0,  -1 },     //UP,
+		{ -1, -1 },     //UP_LEFT,
+		{ -1,  0 },     //LEFT,
+		{ -1,  1 },     //DOWN_LEFT,
+		{ 0,   1 },     //DOWN,
+		{ 1,   1 },     //DOWN_RIGHT,
+		{ 1,   0 },     //RIGHT,
+		{ 1,  -1 },     //UP_RIGHT,
+};
 
 // コンストラクタ
 CStoneManager::CStoneManager()
@@ -9,7 +23,7 @@ CStoneManager::CStoneManager()
 }
 
 // カーソル移動
-void CStoneManager::MoveCursor()
+void CStoneManager::MoveCursor(CBoardManager* cBoard, CTurnManager* cTurn)
 {
 	// キー入力受付
 	switch (_getch())
@@ -34,5 +48,110 @@ void CStoneManager::MoveCursor()
 			// 下に移動
 			cursorY++;
 			break;
+
+		case'\r':
+
+			// 石が置けるか判別
+			if (CanPutStone(cBoard, cTurn))
+			{
+				// ターン切り替え
+				cTurn->ChangeTurn();
+			}
+			break;
 	}
+}
+
+// ボードに石を置く
+//void CStoneManager::PutStone()
+void CStoneManager::PutStone(CBoardManager* cBoard, CTurnManager* cTurn)
+{
+	// ボードに現在のターンの色を格納する
+	cBoard->SetBoard(cursorY, cursorX, cTurn->GetTurn());
+
+	// 石チェック？
+
+	cTurn->ChangeTurn();
+}
+
+// BoardManagerに定義しているものとうまく合わせる
+bool CStoneManager::CanPutStone(CBoardManager* CBoard, CTurnManager* cTurn)
+{
+	//カーソルの箇所に石があるか判別
+	if (CBoard->GetBoard(cursorY, cursorX) != NONE)
+	{
+		// 置かれている場合
+
+		return false;
+	}
+
+	bool isPut = false;
+
+	for (int i = 0; i < directionTypes; i++)
+	{
+		int x = cursorX;
+		int y = cursorY;
+
+		// 隣接する座標を取得
+		x += directions[i][xDirection];
+		y += directions[i][yDirection];
+
+		// 隣のボードに同じ色の石が置いてあるか判別
+		if (CBoard->GetBoard(y, x) != (cTurn->GetTurn() ^ 1))
+		{
+			// 置いてある場合
+
+			continue;
+		}
+
+		while (1)
+		{
+			// 隣接する座標を取得
+			x += directions[i][xDirection];
+			y += directions[i][yDirection];
+
+			// 確認する座標が画面外か判別
+			if ((x < 0) || (x > BOARD_WEIGHT) || (y < 0) || (y >= BOARD_HEIGHT))
+			{
+				break;
+			}
+
+			// 石が置いてあるか判別
+			if (CBoard->GetBoard(y, x) == NONE)
+			{
+				// 置いていない場合
+
+				break;
+			}
+			
+			// 現在のターンで指定されている色と同じ色か判別
+			if (CBoard->GetBoard(y, x) == cTurn->GetTurn())
+			{
+				// ひっくり返し開始地点
+				int turnPosX = cursorX;
+				int turnPosY = cursorY;
+
+				while (1)
+				{
+					// 現在のターンの色の石を置く
+					CBoard->SetBoard(turnPosY, turnPosX, cTurn->GetTurn());
+
+					// 次の座標を算出
+					turnPosX += directions[i][xDirection];
+					turnPosY += directions[i][yDirection];
+
+					// ひっくり返し終点か判別
+					if ((turnPosX == x) && (turnPosY == y))
+					{
+						// 終点の場合
+
+						// フラグ更新
+						isPut = true;
+						break;
+					}
+				}
+			}
+			break;
+		}
+	}
+	return isPut;
 }
--- Othello/StoneManager.h
+++ Othello/StoneManager.h
@@ -1,23 +1,52 @@
 ﻿#pragma once
 
+class CBoardManager;
+class CTurnManager;
+
+// 方向
+enum EDirection
+{
+	UP,
+	UP_LEFT,
+	LEFT,
+	DOWN_LEFT,
+	DOWN,
+	DOWN_RIGHT,
+	RIGHT,
+	UP_RIGHT,
+};
+
+// 方向の種類の数
+const int directionTypes = UP_RIGHT + 1;
+
+// x方向
+const int xDirection = 0;
+// y方向
+const int yDirection = 1;
 
 class CStoneManager
 {
+private:
+	CBoardManager* cBoard;
+	CTurnManager* cTurn;
+
 public:
 	// コンストラクタ
 	CStoneManager();
 	// カーソル移動処理
-	void MoveCursor();
+	void MoveCursor(CBoardManager* cBoard, CTurnManager* cTurn);
+	//void MoveCursor();
+	// 石を置く処理
+	void PutStone(CBoardManager* CBoard, CTurnManager* cTurn);
+	// 石が置けるか判別
+	bool CanPutStone(CBoardManager* CBoard, CTurnManager* cTurn);
 	// cursorXアクセサ
 	int GetCursorX() { return cursorX; };
 	// cursorYアクセサ
 	int GetCursorY() { return cursorY; };
-
 private:
 	// カーソルのx座標
 	int cursorX;
 	// カーソルのy座標
 	int cursorY;
 };
-
-
