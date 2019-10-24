9ab2111dc90559210383fc047aaeeaa476602c1b
Tatyumi <s150204@nishitech.ac.jp>
2019-10-24 22:35:44 +0900

    CturnManager追加
    ターン制御の処理を追加したため、main関数内で使用するよ修正した。

--- Othello/Othello.cpp
+++ Othello/Othello.cpp
@@ -1,11 +1,13 @@
 ﻿#include <stdlib.h>
 #include "BoardManager.h"
 #include "StoneManager.h"
+#include "TurnManager.h"
 
 int main()
 {
 	CBoardManager cBoard;
 	CStoneManager cStone;
+	CTurnManager  cTurn;
 
 	while (1)
 	{
@@ -13,14 +15,14 @@ int main()
 		// 画面クリア
 		system("cls");
 
-		// ボード初期化
-		cBoard.Initialize();
-
 		// ボード表示
 		cBoard.DispBoard(&cStone);
+		// ターン表示
+		cTurn.DispTurn();
 
 		// カーソル移動
-		cStone.MoveCursor();
+		cStone.MoveCursor(&cBoard, &cTurn);
 	}
+
 	return 0;
 }
